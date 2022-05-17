fe3d7e69b3bca5380a6e33abde75c328e57f7ce4
Aryan <aryan19@gmail.com>
2022-05-17 14:11:50 +0530

    Aryan Bhardwaj changed sum of 2 numbers

--- Sum of 2 numbers.cpp
+++ Sum of 2 numbers.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-    int num1, num2, sum;
-    cout << "\n sum of two numbers : \n";
-    cout << "Input 1st number : ";
-    cin >> num1;
-    cout << "Input 2nd number : ";
-    cin >> num2;
-    sum = num1 + num2;
+    int NUM1, NUM2, sum;
+    cout << "\n sum of two Numbers : \n";
+    cout << "Input 1st Number : ";
+    cin >> NUM1;
+    cout << "Input 2nd Number : ";
+    cin >> NUM2;
+    sum = NUM1 + NUM2;
     cout << sum << endl;
     cout << endl;
     return 0;
