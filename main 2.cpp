6c84fb82cdd6699e6cc2c9cf92e94547985c6d3b
Aryan Rawat <adroit.aryan.rawat@gmail.com>
2022-05-17 13:15:32 +0530

    C++ commit

---
+++ main 2.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  ifeslse
+//
+//  Created by Aryan Rawat on 12/03/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    cout<<"INput First number";
+    cin>>num1;
+    if(num1>0){
+        cout<<"number="<<num1;
+    }
+    else{
+        cout<<"Negative number"<<endl;
+    }
+    cout<<endl; 
+    return 0;
+}
