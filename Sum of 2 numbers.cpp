c7f42393c205d65f8f3086aebec2b66ebda8afb7
Aryan <aryan19@gmail.com>
2022-05-17 13:49:14 +0530

    Made by Aryan Bhardwaj

---
+++ Sum of 2 numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, sum;
+    cout << "\n sum of two numbers : \n";
+    cout << "Input 1st number : ";
+    cin >> num1;
+    cout << "Input 2nd number : ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << sum << endl;
+    cout << endl;
+    return 0;
+}
